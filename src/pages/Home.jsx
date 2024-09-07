aa1326ffe10c172ff0b516220803e67f3a541d43
Japneet Rajput <japneetsingh.rajput@iss-stoxx.com>
2024-09-07 14:28:17 +0530

    Reverted back to links on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Weather } from './Weather'
 
 export const Home = () => {
     return (
         <>
-            <Weather />
-            {/* <div>Home</div>
+            <div>Home</div>
             <a style={{ display: 'block' }} href='/weather'>Weather</a>
-            <a href='/todo'>ToDo</a> */}
+            <a href='/todo'>ToDo</a>
         </>
     )
 }
