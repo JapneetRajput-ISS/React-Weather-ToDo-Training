29fb945aa0e12ca2f7f664f56cf13ab6dac6ac1d
Japneet Rajput <japneetsingh.rajput@iss-stoxx.com>
2024-09-07 13:31:21 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Home } from './pages/Home';
 import { ToDo } from './pages/ToDo';
 import { Weather } from './pages/Weather';
 import { Routes, Route } from "react-router-dom";
@@ -5,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/weather" element={<Weather />} />
       <Route path="/todo" element={<ToDo />} />
     </Routes>
